b20d09b2fac0b1ad8f5364142fc1ed82791c8771
Zephinax <Zephinax@gmail.com>
2024-03-01 22:25:48 +0330

    add gallery - fix js - some extra futures

--- assets/js/script.js
+++ assets/js/script.js
@@ -32,11 +32,11 @@ window.addEventListener('scroll', () => {
 const navToggleBtn = document.querySelector('[data-nav-toggle-btn]');
 const navbar = document.querySelector('[data-navbar]');
 
-navToggleBtn.addEventListener('click', () => {
+function toggleNav() {
   elemToggleFunc(navToggleBtn);
   elemToggleFunc(navbar);
   elemToggleFunc(document.body);
-});
+}
 
 /**
  * skills toggle
